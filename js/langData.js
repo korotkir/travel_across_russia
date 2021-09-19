3b570e2793d5de0022579abab97badba15773be1
tre3ubec <ekoroteev27@yandex.ru>
2021-09-19 19:06:24 +0300

    added utils

---
+++ js/langData.js
@@ -0,0 +1,20 @@
+const ru = {
+ 'leadTitle' : 'Путешествия по России',
+ 'leadSubtitle' : 'Настоящая страна не в выпусках новостей, а здесь.' ,
+ 'leadCaption' : 'ваша полка — верхняя',
+ 'introTitle' : 'Чего мы там не видели?',
+ 'placeTitle' : ['Куршская коса', 'Кольский', 'Алтай', 'Зимний байкал', 'Карелия'],
+ 'coverTitle' : 'До Байкала «на собаках»',
+ 'footerItem' : ['Карты', 'Погода', 'Расписание', 'Календарь', 'Путешествия'],
+ 'footerCopyright' : '© 2021. Коротееев К.'
+}
+const en = {
+ 'leadTitle' : 'Traveling in Russia',
+ 'leadSubtitle' : 'The real country is not on the news, but here.' ,
+ 'leadCaption' : 'your shelf is top',
+ 'introTitle' : 'What didn\'t we see there?',
+ 'placeTitle' : ['Curonian Spit', 'Kola', 'Altai', 'Winter Baikal', 'Karelia'],
+ 'coverTitle' : 'To Lake Baikal\' on the dogs',
+ 'footerItem' : ['Maps', 'Weather', 'Schedule', 'Calendar', 'Travel'],
+ 'footerCopyright' : '© 2021. Koroteev K..'
+}
---
+++ js/utils.js
@@ -0,0 +1,3 @@
+let antiCamelCase = (el) => {
+ return el.replace(el[el.search(/\w[A-Z]/g)], el[el.search(/\w[A-Z]/g)] + '__').toLowerCase()
+}
