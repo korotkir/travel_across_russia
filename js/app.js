436f703ddf505a369cbb99218442510a20a32176
tre3ubec <ekoroteev27@yandex.ru>
2021-09-27 21:07:44 +0300

    add new functional

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
  const rus = document.querySelector('.header__langLinkRu')
  const eng = document.querySelector('.header__langLinkEng')
+ const introNumber = Object.values(document.querySelectorAll('.intro__number'))
+
+ console.log(introNumber.map(el => el.textContent))
 
  rus.addEventListener('click', translator(rusDict))
  eng.addEventListener('click', translator(engDict))
@@ -16,6 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
      : typeof Object.values(dict)[index] !== 'string'
       ? Object.values(document.querySelectorAll(antiCamelCase(element)))
        .map((el, i) => el.textContent = dict[element][i]) : '')
+
+   Object.values(document.querySelectorAll('.intro__fact'))
+    .map(el => el.insertAdjacentHTML('beforeend', `<span class="intro__number">${introNumber.textContent}</span>`))
+
   }
  }
 
